6ac4e33a6f2c7860a0b424d3083e41b203b8ae2b
FranciscoVasconcelos-pt <1240934@isep.ipp.pt>
2026-03-02 21:10:28 +0000

    make_dataset.py done, pkl file created

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -71,17 +71,79 @@ for f in files:
 # --------------------------------------------------------------
 # Working with datetimes
 # --------------------------------------------------------------
+acc_df.index = pd.to_datetime(acc_df["epoch (ms)"], unit="ms")
+gyro_df.index = pd.to_datetime(gyro_df["epoch (ms)"], unit="ms")
 
-
+acc_df.drop(["epoch (ms)", "time (01:00)"], axis=1)
+gyro_df.drop(["epoch (ms)", "time (01:00)"], axis=1)
 # --------------------------------------------------------------
 # Turn into function
 # --------------------------------------------------------------
+files = glob("../../data/raw/MetaMotion/MetaMotion/*.csv")
+
+
+def data_from_files(files):
+    acc_df = pd.DataFrame()
+    gyro_df = pd.DataFrame()
+
+    acc_set = 1  # counter individual
+    gyro_set = 1
+
+    for f in files:
+        filename = os.path.basename(f)  # remove o caminho
+        participant = filename.split("-")[0]
+        label = filename.split("-")[1]
+        category = filename.split("-")[2].rstrip("123").rstrip("_MetaWear_2019")
+
+        df = pd.read_csv(f)
+
+        df["participant"] = participant
+        df["label"] = label
+        df["category"] = category
+
+        if "Accelerometer" in f:
+            df["set"] = acc_set
+            acc_set += 1
+            acc_df = pd.concat(
+                [acc_df, df]
+            )  # concat junta o df ao acc_df e constroi o df
+
+        if "Gyroscope" in f:
+            df["set"] = gyro_set
+            gyro_set += 1
+            gyro_df = pd.concat([gyro_df, df])
+
+    acc_df.index = pd.to_datetime(acc_df["epoch (ms)"], unit="ms")
+    gyro_df.index = pd.to_datetime(gyro_df["epoch (ms)"], unit="ms")
+
+    acc_df.drop(
+        ["epoch (ms)", "time (01:00)", "elapsed (s)"], axis=1, inplace=True
+    )  # inplace apaga as colunas do DF diretamente
+    gyro_df.drop(["epoch (ms)", "time (01:00)", "elapsed (s)"], axis=1, inplace=True)
+
+    return acc_df, gyro_df
+
+
+acc_df, gyro_df = data_from_files(files)
 
 
 # --------------------------------------------------------------
 # Merging datasets
 # --------------------------------------------------------------
 
+data_merged = pd.concat([acc_df.iloc[:, :3], gyro_df], axis=1)
+data_merged.columns = [
+    "acc_x",
+    "acc_y",
+    "acc_z",
+    "gyro_x",
+    "gyro_y",
+    "gyro_z",
+    "label",
+    "category",
+    "participant",
+    "set",
+]
 
 # --------------------------------------------------------------
 # Resample data (frequency conversion)
@@ -89,6 +151,28 @@ for f in files:
 
 # Accelerometer:    12.500HZ
 # Gyroscope:        25.000Hz
+
+sampling = {
+    "acc_x": "mean",
+    "acc_y": "mean",
+    "acc_z": "mean",
+    "gyro_x": "mean",
+    "gyro_y": "mean",
+    "gyro_z": "mean",
+    "label": "last",
+    "category": "last",
+    "participant": "last",
+    "set": "last",
+}
+
+# Reamostragem do df principal
+data_merged[:1000].resample(rule="200ms").apply(sampling)
+
+days = [g for n, g in data_merged.groupby(pd.Grouper(freq="D"))]
+
+data_resampled = pd.concat(
+    [df.resample(rule="200ms").apply(sampling).dropna() for df in days]
+)
 
 
 # --------------------------------------------------------------
