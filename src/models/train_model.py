a42062b7862c2c4513a48997419064b85994cda7
FranciscoVasconcelos-pt <1240934@isep.ipp.pt>
2026-04-20 12:44:31 +0100

    feature splitting for model training and testing

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -14,22 +14,69 @@ plt.rcParams["figure.figsize"] = (20, 5)
 plt.rcParams["figure.dpi"] = 100
 plt.rcParams["lines.linewidth"] = 2
 
-
+df = pd.read_pickle("../../data/interim/03_data_features.pkl")
 # --------------------------------------------------------------
 # Create a training and test set
 # --------------------------------------------------------------
+df_train = df.drop(
+    ["participant", "category", "set"], axis=1
+)  # nao adicionam valor ao df, ao fazer o modelo
+X = df_train.drop("label", axis=1)
+Y = df_train["label"]
 
+X_train, X_test, Y_train, Y_test = train_test_split(
+    X, Y, test_size=0.25, random_state=42, stratify=Y
+)  # stratify separa melhor a data dado o y
 
 # --------------------------------------------------------------
 # Split feature subsets
 # --------------------------------------------------------------
+basic_features = ["acc_x", "acc_y", "acc_z", "gyro_x", "gyro_y", "gyro_z"]
+square_features = ["acc_r", "gyro_r"]
+PCA_features = ["pca_1", "pca_2", "pca_3"]
+time_features = [f for f in df_train.columns if "_temp_" in f]
+freq_features = [f for f in df_train.columns if ("_freq" in f) or ("_pse" in f)]
+cluster_features = ["cluster"]
 
+print("basic features:", len(basic_features))
+print("square features:", len(square_features))
+print("PCA features:", len(PCA_features))
+print("time features:", len(time_features))
+print("freq features:", len(freq_features))
+print("cluster features:", len(cluster_features))
 
+feature_set_1 = basic_features
+feature_set_2 = list(set(basic_features + square_features))
+feature_set_3 = list(set(feature_set_2 + time_features))
+feature_set_4 = list(set(feature_set_3 + freq_features + cluster_features))
 # --------------------------------------------------------------
 # Perform forward feature selection using simple decision tree
 # --------------------------------------------------------------
+learner = ClassificationAlgorithms()
 
+max_features = 10
+selected_features, ordered_features, ordered_scores = learner.forward_selection(
+    max_features, X_train, Y_train
+)
 
+selected_features = [
+    "pca_1",
+    "duration",
+    "acc_x_freq_0.0_Hz_ws_10",
+    "acc_y_freq_0.0_Hz_ws_10",
+    "gyro_z_freq_weighted",
+    "acc_r_temp_mean_ws_5",
+    "acc_r_freq_2.5_Hz_ws_10",
+    "gyro_y_freq_1.5_Hz_ws_10",
+    "acc_y",
+    "gyro_r_freq_1.5_Hz_ws_10",
+]
+plt.figure(figsize=(10, 5))
+plt.plot(np.arange(1, max_features + 1, 1), ordered_scores)
+plt.xlabel("Number of features")
+plt.ylabel("Accuracy")
+plt.xticks(np.arange(1, max_features + 1, 1))
+plt.show()
 # --------------------------------------------------------------
 # Grid search for best hyperparameters and model selection
 # --------------------------------------------------------------
